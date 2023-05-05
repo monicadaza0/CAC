9fab1d1dff627ede7b9982a2a8273ee9e9ffa852
monicadaza0 <132624491+monicadaza0@users.noreply.github.com>
2023-05-04 19:04:38 -0500

    TA1_inicio del Menú hecho en clase
    Inicio del código del proyecto cac en donde se definieron variables para el menú y se creo el mensaje de bienvenida.

---
+++ proyecto cac.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    char departamento;
+    char amazonas
+    char antioquia
+    char arauca
+    char atlantico
+    char bogota
+    char bolivar
+    char boyaca
+    char caldas
+    char caqueta
+    char casanare
+    char cauca
+    char cesar
+    char choco
+    char cordoba
+    char cundinamarca
+    char guainia
+    char guaviare
+    char huila
+    char la guajira
+    char magdalena
+    char meta
+    char nariño
+    char norte de santander
+    char putumayo
+    char quindio
+    char risaralda
+    char san andres y providencia
+    char santander 
+    char sucre
+    char tolima 
+    char valle del cauca
+    char vaupes
+    char vichada
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   printf("usuario sea usted bienvenido a nuestro programa.\n este esta diseñado con el proposito de dar a conocer las cifras \n de contaminacion atmosferica por departamento a nivel nacional actualmente\n");
+   printf("porfavor digite el departamento a consultar:");
+   scanf("%c",departamento);
+   printf(departamento);
+   
+
+    
+}

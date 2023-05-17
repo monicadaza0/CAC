8b5b22d23b1ddb0529cc53a094283fb2e3d9e55d
monicadaza0 <132624491+monicadaza0@users.noreply.github.com>
2023-05-16 20:02:21 -0500

    2 codigo

--- proyecto cac.c
+++ proyecto cac.c
@@ -1,58 +1,29 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 
 int main()
 {
+    int numero = 0;
+    int start = 1;
     char departamento;
-    char amazonas
-    char antioquia
-    char arauca
-    char atlantico
-    char bogota
-    char bolivar
-    char boyaca
-    char caldas
-    char caqueta
-    char casanare
-    char cauca
-    char cesar
-    char choco
-    char cordoba
-    char cundinamarca
-    char guainia
-    char guaviare
-    char huila
-    char la guajira
-    char magdalena
-    char meta
-    char nariño
-    char norte de santander
-    char putumayo
-    char quindio
-    char risaralda
-    char san andres y providencia
-    char santander 
-    char sucre
-    char tolima 
-    char valle del cauca
-    char vaupes
-    char vichada
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
    printf("usuario sea usted bienvenido a nuestro programa.\n este esta diseñado con el proposito de dar a conocer las cifras \n de contaminacion atmosferica por departamento a nivel nacional actualmente\n");
-   printf("porfavor digite el departamento a consultar:");
-   scanf("%c",departamento);
-   printf(departamento);
+  
    
-
+   while(start) {
+    printf("[1] digite el departamento a consultar:");
+    printf("[2] cerrar programa:");
+    scanf("%d",&numero);
+    start = 0;
+   }
+   
     
+   if(numero == 1 ){
+    printf("por favor digite el departamento");
+    scanf("%c",departamento);
+   }
+
+    //system ("pause");
+    return 0;
 }
